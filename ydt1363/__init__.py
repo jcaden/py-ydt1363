12c4a4e2a65f67186fb72b3a3389c7938064e256
Jose Antonio Santos Cadenas <santoscadenas@gmail.com>
2026-01-04 14:50:27 +0100

    Release 0.1.1

--- ydt1363/__init__.py
+++ ydt1363/__init__.py
@@ -4,7 +4,7 @@ ydt1363: Python implementation for YDT 1363 protocol.
 This library provides functionality to work with the YDT 1363 protocol.
 """
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "José Antonio Santos Cadenas"
 __all__ = ["BMSProtocol", "BMSFrame"]
 
